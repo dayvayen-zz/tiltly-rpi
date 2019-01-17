805155fa17b854f87e71ec599cf83a9ca5b6eadb
dayvayen <daniellevnelson@gmail.com>
2019-01-17 15:48:54 -0800

    initial test script

--- control.py
+++ control.py
@@ -1,8 +1,31 @@
 from os import uname
 from socket import gethostname
+import json
 import sys
 import time
 import tilt
+import plotly.plotly as py # plotly library
+from plotly.graph_objs import *
+import datetime as dt
+
+with open('./config.json') as config_file:
+    plotly_user_config = json.load(config_file)
+
+    py.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+
+url = py.plot([
+    {
+        'x': [], 'y': [], 'type': 'scatter',
+        'stream': {
+            'token': plotly_user_config['plotly_streaming_tokens'][0],
+            'maxpoints': 200
+        }
+    }], filename='Raspberry Pi Streaming Example Values')
+
+print "View your streaming graph here: ", url
+
+stream = py.Stream(plotly_user_config['plotly_streaming_tokens'][0])
+stream.open()
 
 beacon = {}
 
@@ -13,16 +36,12 @@ def getTemp():
 def getGravity():
     return beacon['Gravity']/1000 if beacon else 1000
 # first let's try just getting a streaming graph with the gravity data.
-global beacon
-beacon = tilt.getFirstTilt()
+# steps to do that:
+# 1. read in the data using tilt.getFirstTilt()
+# 2. get the gravity with getGravity
+# 3. stream to dash?
+# maybe i could collect the data and update the plot every so often?
 
-variables = {
-        'Gravity': {
-            'type': 'numeric',
-            'bind': getGravity
-        },
-        'Beer Temp': {
-            'type': 'numeric',
-            'bind': getTemp
-        }
-    }
+while True:
+    tilt_data = tilt.getFirstTilt()
+    stream.write({'x': dt.datetime.now(), 'y': tilt_data['Temp']})
