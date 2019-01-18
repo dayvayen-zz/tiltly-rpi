b0b805ce5404fbb54ff607d656282b9eec43d712
dayvayen <daniellevnelson@gmail.com>
2019-01-18 10:28:32 -0800

    trying the plotly api again

--- test.py
+++ test.py
@@ -5,12 +5,38 @@ import sys
 import time
 import tilt
 import datetime as dt
-import plotly
-plotly.offline.init_notebook_mode(connected=True)
-import plotly.offline as py
+import plotly.plotly as py # plotly library
+from plotly.graph_objs import Scatter, Layout, Figure # plotly graph objects
+import time
+
+py.sign_in(username, api_key)
+
+username = 'dayvayen'
+api_key = 'cxKRDUgmdRm70fXKPNW3'
+stream_token = '231cxdk5rc'
+
+trace1 = Scatter(
+    x=[],
+    y=[],
+    stream=dict(
+        token=stream_token,
+        maxpoints=200
+    )
+)
+
+layout = Layout(
+    title='Raspberry Pi Streaming Sensor Data'
+)
+
+fig = Figure(data=[trace1], layout=layout)
+
+print py.plot(fig, filename='Raspberry Pi Streaming Example Values')
+
+stream = py.Stream(stream_token)
+stream.open()
 
 while True:
     beacon = tilt.getFirstTilt()
     now = dt.datetime.now()
-    print({'time': now, 'gravity': beacon['Gravity']})
-    time.sleep(2)
+    stream.write({'x': now, 'y': beacon['Gravity']})
+    time.sleep(0.25)
