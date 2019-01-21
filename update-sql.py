9b7e670716c4450bd5ec7776c3010bb36d8bfcf7
dayvayen <daniellevnelson@gmail.com>
2019-01-21 10:39:18 -0800

    seeing if sql and tilt can connect

--- update-sql.py
+++ update-sql.py
@@ -1,8 +1,10 @@
 import sqlite3
 from sqlite3 import Error
 import tilt
+import datetime as dt
 
 db_file = raw_input("Enter database name: ")
+beerName = raw_input("Enter beer table name: ")
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -16,3 +18,27 @@ def create_connection(db_file):
         print(e)
 
     return None
+
+    def
+
+def add_data(conn, beerName, data):
+    """ take beer data from tilt and add it
+        to the table
+    """
+    sql = "insert into " + beerName + """ (time,
+                                            temperature,
+                                            gravity)
+            VALUES(?,?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, beerName)
+    return cur.lastrowid
+
+def main():
+    conn = create_connection(db_file)
+    beacon = tilt.getFirstTilt()
+    with conn:
+        data = (dt.datetime.now(), beacon['Temp'], beacon['Gravity'])
+        add_data(conn, beerName, data)
+
+if __name__ == '__main__':
+    main()
