2997177424df47a392845c46567e129791a99787
dayvayen <daniellevnelson@gmail.com>
2019-01-21 11:05:43 -0800

    trying out timeloop

--- update_at_intervals.py
+++ update_at_intervals.py
@@ -1,10 +1,12 @@
 import updateSQL
 import time
+from timeloop import Timeloop
 
 db_file = raw_input("Enter database name: ")
 beerName = raw_input("Enter beer table name: ")
 interval = float(raw_input("How many minutes between updates? ") * 60
 
-while True:
-    updateBeerTable()
-    time.sleep(interval)
+tl = Timeloop()
+
+@tl.job(interval = timedelta(seconds = interval))
+updateBeerTable()
