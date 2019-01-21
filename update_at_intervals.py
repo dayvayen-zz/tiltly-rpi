53d0e086ef0763442e2dc761ca505f64a0b9b492
dayvayen <daniellevnelson@gmail.com>
2019-01-21 10:58:56 -0800

    changing some names

--- update_at_intervals.py
+++ update_at_intervals.py
@@ -1,4 +1,4 @@
-import update-sql
+import updateSQL
 import time
 
 db_file = raw_input("Enter database name: ")
@@ -8,4 +8,3 @@ interval = float(raw_input("How many minutes between updates? ") * 60
 while True:
     updateBeerTable()
     time.sleep(interval)
-    
